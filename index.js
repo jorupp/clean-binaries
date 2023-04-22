22b6b511c5d4869249b8d28bf6311c289fce3600
Jonathan Rupp <jrupp@rightpoint.com>
2023-04-22 11:32:12 -0500

    quick script to find binary-like things that can be deleted

---
+++ index.js
@@ -0,0 +1,28 @@
+import { readdir, stat } from 'fs/promises';
+import nodePath from 'path';
+import process from 'node:process';
+
+const path = process.argv[2];
+const toMatch = [
+    'bin',
+    'obj',
+    'packages',
+    'node_modules',
+    'bower_components',
+    '.venv'
+]
+
+async function process(path) {
+    const children = await readdir(path, { withFileTypes: true });
+    for(const i of children) {
+        if (!i.isDirectory()) continue;
+        const child = nodePath.join(path, i.name);
+        if (toMatch.includes(i.name)) {
+            console.log(child);
+        } else {
+            process(child);
+        }
+    }
+}
+
+await process(path);
